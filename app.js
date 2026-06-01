5495908d6296e2941c9f2bfdfb64349eb4de51c3
Kevkidev <7610777+kevkidev@users.noreply.github.com>
2026-06-01 21:53:53 +0200

    ok

--- app.js
+++ app.js
@@ -2,19 +2,69 @@ function $(elementId) {
     return document.getElementById(elementId);
 }
 
-$("btn").onclick = function () {
+function $int(elementId) {
+    return parseInt($(elementId).value);
+}
 
-    const start = parseInt($("start_hour").value) * 60 + parseInt($("start_min").value);
-    const stop = parseInt($("stop_hour").value) * 60 + parseInt($("stop_min").value);
-    const tot = stop - start - parseInt($("break").value);
+function calculateWorkTime() {
 
-    const sup = tot - 7 * 60;
-    const supHour = Math.trunc(sup / 60);
-    const supMin = Math.round((sup / 60 - supHour) * 60);
+    const base = getMinutes($int("base_hour"), $int("base_min"));
+    const start = getMinutes($int("start_hour"), $int("start_min"))
+    const breakk = getMinutes($int("break_hour"), $int("break_min"))
+    const stop = getMinutes($int("stop_hour"), $int("stop_min"))
+
+    const tot = stop - start - breakk;
+    const sup = getHours(tot - base);
+
+    $("result_sup").innerText = "";
+    $("result_tot").innerText = getHours(tot).toStringDuration;
+    if (sup.hour < 0 || sup.min < 0) {
+        $("result_sup").innerText = "-"
+    }
+    console.log(sup);
+
+    $("result_sup").innerText += Math.abs(sup.hour) + "h" + Math.abs(sup.min);
+
+}
+
+
+function calculateEndTime() {
+
+    const base = getMinutes($int("base_hour"), $int("base_min"));
+    const start = getMinutes($int("start_hour"), $int("start_min"))
+    const breakk = getMinutes($int("break_hour"), $int("break_min"))
+
+    const stop = start + breakk + base;
+    const stopHours = getHours(stop);
+    $("simu_end_hours").innerText = "Fin prévue à " + stopHours.toStringTime;
+}
+
+
+function calculateSleepTime() {
+
+    const sleep = parseInt($("sleep_hour").value) * 60 + parseInt($("sleep_min").value);
+    const wake = parseInt($("wake_hour").value) * 60 + parseInt($("wake_min").value);
+    let tot = wake - sleep;
+    if (tot <= 0) tot += 12;
 
     const toHour = Math.trunc(tot / 60);
     const toMin = Math.round((tot / 60 - toHour) * 60);
 
-    $("result_tot").innerText = toHour + "H" + toMin;
-    $("result_sup").innerText = supHour + "H" + supMin;
+    $("bed_time").innerText = toHour + "h" + toMin;
+}
+
+function getHours(minutes) {
+    const hour = Math.trunc(minutes / 60);
+    const min = Math.round((minutes / 60 - hour) * 60);
+    const minString = min < 10 ? "0" + min : min
+    return {
+        hour,
+        min,
+        toStringTime: hour + ":" + minString,
+        toStringDuration: hour + "h" + minString,
+    }
+}
+
+function getMinutes(hour, minutes) {
+    return hour * 60 + minutes;
 }
